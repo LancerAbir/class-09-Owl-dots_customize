9e17a7088938b0e03e2b4da697cad3e25be31ca5
abir2665266 <dhar.abir.kumar@gmail.com>
2017-05-07 00:27:36 +0600

    class 09
    Owl-dots_customize

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function($){
+    'use strict';
+
+    $(".client_testimonials_list").owlCarousel({
+        items : 3,
+        autoplay : false,
+        loop : true,
+        nav : false,
+        dots : true,
+        margin : 30,
+    });
+
+
+
+
+
+
+
+
+
+    
+})(jQuery);
